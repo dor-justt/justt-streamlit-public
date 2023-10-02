98b2454f6067f944ef0f04d185e3e9218345394d
Dor <dor@Dor-B-Mac.local>
2023-10-02 11:36:17 +0300

    .

--- hotel-pdf-parser/data_post_processor.py
+++ hotel-pdf-parser/data_post_processor.py
@@ -12,5 +12,33 @@ class DataPostProcessor:
         processed_result = {getattr(FIELD_NAMES, f.name).outer: extracted_data.get(getattr(FIELD_NAMES, f.name).inner, '') for f in
                             fields(FIELD_NAMES)}
         processed_result[FIELD_NAMES.CHARGEBACK_ID.outer] = chargeback_id
+        processed_result = DataPostProcessor._verify_names(processed_result)
         df = pd.Series(processed_result).to_frame().T
         return processed_result, df
+
+    @staticmethod
+    def _verify_names(processed_result) -> Dict[str, str]:
+        # fill first name by full name
+        if len(processed_result[FIELD_NAMES.GUEST_FULL_NAME.outer]) > 0 and processed_result[FIELD_NAMES.GUEST_FIRST_NAME.outer] == "":
+            processed_result[FIELD_NAMES.GUEST_FIRST_NAME.outer] = processed_result[FIELD_NAMES.GUEST_FULL_NAME.outer].split()[0]
+
+        # fill last name by full name
+        if len(processed_result[FIELD_NAMES.GUEST_FULL_NAME.outer]) > 0 and processed_result[FIELD_NAMES.GUEST_LAST_NAME.outer] == "":
+            processed_result[FIELD_NAMES.GUEST_FIRST_NAME.outer] = processed_result[FIELD_NAMES.GUEST_FULL_NAME.outer].split()[-1]
+            
+        # fill full name by first and last name
+        if len(processed_result[FIELD_NAMES.GUEST_FULL_NAME.outer]) == 0:
+            # use both first and last name
+            if len(processed_result[FIELD_NAMES.GUEST_FIRST_NAME.outer]) > 0 and \
+                    len(processed_result[FIELD_NAMES.GUEST_LAST_NAME.outer]) > 0:
+                processed_result[FIELD_NAMES.GUEST_FULL_NAME.outer] = processed_result[FIELD_NAMES.GUEST_FIRST_NAME.outer] + " " + \
+                                                                      processed_result[FIELD_NAMES.GUEST_LAST_NAME.outer]
+            # use first name only
+            elif len(processed_result[FIELD_NAMES.GUEST_FIRST_NAME.outer]) > 0 and \
+                    len(processed_result[FIELD_NAMES.GUEST_LAST_NAME.outer]) == 0:
+                processed_result[FIELD_NAMES.GUEST_FULL_NAME.outer] = processed_result[FIELD_NAMES.GUEST_FIRST_NAME.outer]
+            # use last name only
+            elif len(processed_result[FIELD_NAMES.GUEST_FIRST_NAME.outer]) == 0 and len(processed_result[FIELD_NAMES.GUEST_LAST_NAME.outer]) > 0:
+                processed_result[FIELD_NAMES.GUEST_FULL_NAME.outer] = processed_result[FIELD_NAMES.GUEST_LAST_NAME.outer]
+
+        return processed_result
