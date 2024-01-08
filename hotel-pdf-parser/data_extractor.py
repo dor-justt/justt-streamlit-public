3c67010955faa702c9f9bcab4fcaabb9ed2528c4
shirakle <shira@justt.ai>
2024-01-08 11:19:21 +0200

    new dps and json output

--- hotel-pdf-parser/data_extractor.py
+++ hotel-pdf-parser/data_extractor.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 class DataExtractor:
     openai.api_key = os.getenv(key='OPENAI_API_KEY')
-    SYSTEM_CONTENT = f"You are an assistant that helps extract data from text which is parsed from PDF invoices." \
+    SYSTEM_CONTENT = f"You are an assistant that helps extract data from text which is parsed from PDF invoices. " \
                      f"You will receive a text, and return a json with the following: " \
                      f"{{{FIELD_NAMES.VENUE_TITLE.inner}: <the full title of the vendor>, " \
                      f"{{{FIELD_NAMES.VENUE_ADDRESS.inner}: <the full address of the vendor>, " \
@@ -28,8 +28,22 @@ class DataExtractor:
                      f"{FIELD_NAMES.BILLING_COUNTRY.inner}: <the billing country of the customer>," \
                      f"{FIELD_NAMES.CHECK_IN_DATE.inner}: <date and time of the check in, in format dd:MM:yyyy HH:mm:ss>, " \
                      f"{FIELD_NAMES.CHECK_OUT_DATE.inner}: <date and time of the check out, in format dd:MM:yyyy HH:mm:ss>, "\
-                     f"{FIELD_NAMES.ORDER_NUMBER.inner}: <Leave this blank>,"\
-                     f"{FIELD_NAMES.TRANSACTION_AMOUNT}: <Leave this blank>}}.\n" \
+                     f"{FIELD_NAMES.ORDER_NUMBER.inner}: <Leave this blank>, "\
+                     f"{FIELD_NAMES.TRANSACTION_AMOUNT.inner}: <Leave this blank>, " \
+                     f"{FIELD_NAMES.RESERVATION_ID.inner}: <the reservation id or booking id. if this information is missing" \
+                     f" leave this blank.>, " \
+                     f"{FIELD_NAMES.COSTUMER_EMAIL.inner}: <the email address of the guest\customer.>, " \
+                     f"{FIELD_NAMES.IS_CHECKED_IN.inner}: <if the costumer checked in return yes if not return no.>, " \
+                     f"{FIELD_NAMES.IS_CONTACTLESS_CHECKED_IN.inner}: <if the costumer checked in via mobile or contactless return yes if the costumer checked in phisically return no.>, " \
+                     f"{FIELD_NAMES.NUMBER_OF_GUESTS.inner}: <the total number of guests>, " \
+                     f"{FIELD_NAMES.NUMBER_OF_NIGHTS.inner}: <the total number of nights>, " \
+                     f"{FIELD_NAMES.ROOM_FEATURES.inner}: <the listing or room features>, " \
+                     f"{FIELD_NAMES.CANCELLATION_DATE.inner}: <if the order was canceled, return the cancellation date>, " \
+                     f"{FIELD_NAMES.TRANSACTION_METHOD.inner}: <the method by which the transaction was processed>, " \
+                     f"{FIELD_NAMES.RETURNING_CUSTOMER.inner}: <an indication if the customer is a returning customer. Will be counted as a returning guest if they made more than 1 order using the same account>, " \
+                     f"{FIELD_NAMES.BOOKING_CHANNEL.inner}: <the channel used by the cardholder to make the purchase/booking>, " \
+                     f"{FIELD_NAMES.CREDITS_USED.inner}: <the amount of credits the customer used in this order>, " \
+                     f"{FIELD_NAMES.CVM.inner}: <the Cardholder Verificaiton Method used (or not used) for the payment>}}.\n" \
                      f"For each value in the json, if you can not get the answer, return an empty string. " \
                      f"Your returned answer must always be a json as described"
 
@@ -40,7 +54,8 @@ class DataExtractor:
             {"role": "user", "content": chunks[0]},  # TODO current path using chunk[0] only
         ]
         chatbot_response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
+            model="gpt-3.5-turbo-1106",
+            response_format={"type": "json_object"},
             temperature=0,
             messages=messages,
         )
