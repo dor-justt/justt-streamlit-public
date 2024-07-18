c1a81f55e044dfeede22478966ab4bdadd414a59
Dor <dor@Dor-B-Mac.local>
2024-07-18 10:54:05 +0300

    .

--- automatic_onboarding/constants.py
+++ automatic_onboarding/constants.py
@@ -1,7 +1,6 @@
-from dataclasses import dataclass, field
 from typing import List
 
-
+OPENAI_MODEL = "gpt-4o"  # "gpt-3.5-turbo-16k"
 REDUCE_HELLUCINATIONS_STR: str = "You can only choose out the options provided above, " \
                                  "dont invent other options. Return the answer as a list of strings, which can be empty if none of the " \
                                  "options apply. Include on ly answers you are certain of."
@@ -25,6 +24,7 @@ class OUTPUTS:
     MERCHANT_NAME = 'merchant_name'
     ORDER_FOR_UI = [MERCHANT_NAME, DESCRIPTION, INDUSTRY, DIRECT_SALES_CHANNELS, BUSINESS_OPERATION, SELLS_THROUGH, BILLING_MODELS,
                     GEOGRAPHICAL_AREAS, WHEN_CUSTOMER_MAKES_A_PAYMENT, TERMS_COVERAGE, INSURANCE_OPTIONS, CUSTOMER_SUPPORT_EMAIL]
+
 
 class ResponseCategories:
     INDUSTRIES: List[str] = ['retail', 'financial services', 'travel', 'gaming', 'crypto', 'software subscription', 'gambling', 'ticketing', 'delivery']
@@ -56,7 +56,10 @@ class QuestionnairePrompts:
                    f'{OUTPUTS.DESCRIPTION}: <Describe the company in a few sentences. Describe in details what does the company sell / offer> ' \
                    f'{OUTPUTS.INDUSTRY}: <See this list of one-word industry descriptions: {ResponseCategories.INDUSTRIES_STR}. ' \
                    f'Choose one description that suits the provided company the most based on the company description. ' \
-                   f'You can only use the options provided above, and do not add explanations.>,' \
+                   f'You can only use the options provided above, and do not add explanations. Note that retail industry includes ' \
+                   f'businesses that sell a variety of goods and services directly to consumers, often through both physical and online ' \
+                   f'stores.>. The gaming industry includes companies that develop, publish, and distribute video games and ' \
+                   f'gaming-related products and services.>, ' \
                    f'{OUTPUTS.BUSINESS_OPERATION}: <See this list of business operations: {ResponseCategories.BUSINESS_OPERATIONS_STR}. ' \
                    f'choose only the options the provided company operates its business at. {REDUCE_HELLUCINATIONS_STR}>,' \
                    f'{OUTPUTS.SELLS_THROUGH}: <See this list of third party options: {ResponseCategories.BUSINESS_OPERATIONS_STR}. ' \
@@ -77,69 +80,3 @@ class QuestionnairePrompts:
                     f'Choose only the options that are offered by the company. {REDUCE_HELLUCINATIONS_STR}>, ' \
                     f'{OUTPUTS.CUSTOMER_SUPPORT_EMAIL}: <What is the merchant customer support email? Return the answer as a string. ' \
                     f'If you can not find it, return NULL>}}'
-    DESCRIPTION: str = 'From the information in this website, and what you know about this merchant, ' \
-                       'answer the following three questions and return a json in the following format: ' \
-                       '{company: answer_to_question_1, ' \
-                       'description: answer_to_question_2, ' \
-                       'long_description: answer_to_question_3}. ' \
-                       '1. Search the company name. Return the answer as a string. ' \
-                       '2. Describe the company in up to five sentences, and no less than three ' \
-                       'sentences. Focus on what they do as a company, and what services they offer. ' \
-                       'Be neutral and descriptive. Return the answer as a string ' \
-                       '3.Describe the company in a few sentences. Describe in details what does the company sell\offer.'
-    OFFERINGS: str = 'See this list of offerings descriptions: ' \
-                     '"Physical Goods, Digital Goods, Software, In-Person Services, Personal Banking, Payment Facilitation, Investment Services, Accommodation, Top Up Services, Crypto Currencies, Gaming, Gambling, NFTs, Hotels, Car Rentals, Flights, Tickets". ' \
-                     'Choose only the primary offerings that ' \
-                     'suits the provided company the most based on the company description. You can only use the options provided ' \
-                     'above, dont invent other options. Return a json in the following format: {offerings: [OFFERING_A, OFFERING_B]}.'
-    INDUSTRY: str = 'See this list of one-word industry descriptions: ' \
-                    '"Retail, Financial Services, Travel, Gaming, Crypto, Software Subscription, Gambling, Ticketing, Delivery". ' \
-                    'Choose one description that suits the provided company the most based on the company description. You can ' \
-                    'only use the options provided above, dont invent other options. Return a json in the following format: {industry: INDUSTRY}.'
-    CHANNELS_BILLINGS_DELIVERY_EMAIL: str = 'From the information in this website, answer the following four questions and return the answers in a json format: ' \
-                                            '{channels: answer_to_question_1, ' \
-                                            'billings: answer_to_question_2, ' \
-                                            'delivery_methods: answer_to_question_3,' \
-                                            'emailAddress: answer_to_question_4}. ' \
-                                            'If the text from the website does not contain required information to answer ' \
-                                            'the question return "NULL". Dont answer based on your previous knowledge. ' \
-                                            '1. See this list of selling channel options: "Website, Mobile app, 3rd party, Phone calls". ' \
-                                            'Based on the information on the website, choose only the options the provided ' \
-                                            'company uses to sell their products. You can only choose out the options provided above, ' \
-                                            'dont invent other options. Return the answer as a list of strings ' \
-                                            '2. See this list of billing models: "One time payment, Subscriptions, Installments". ' \
-                                            'out of this list, choose only the options the provided company offers. ' \
-                                            'You can only choose out the options provided above, dont invent other options. ' \
-                                            'Choose as many answers as applicable, but not options that are not listed. Return the answer as a list of strings. ' \
-                                            '3. Does the merchant sell physical things? ' \
-                                            'If the merchant doesnt sell physical things return ["Merchant doesnt sell physical goods"]. ' \
-                                            'If he does, what delivery methods does the seller offer? Shipping, in store pickup or other? ' \
-                                            'Return the answer as a list of strings. ' \
-                                            '4. What is the merchant customer support email? Return the answer as a string.'
-    POLICIES: str = 'From the information in this website, answer the following three questions and return the answers in a json format: ' \
-                    '{cancellation: answer_to_question_1, ' \
-                    'refund_policy: answer_to_question_2, ' \
-                    'return_policy: answer_to_question_3}. ' \
-                    'If the text from the website does not contain required information to answer the question replace ' \
-                    'X with "NULL". Dont answer based on your previous knowledge. ' \
-                    '1.Summarize the cancellation policy on the website. Mention the specific conditions for cancellation. ' \
-                    'Provide the relevant quotes from the website and the URL for the source you use for your answer. ' \
-                    'Return the answer as a json in this format: {summary: X, quote: X , source: X}' \
-                    '2. Summarize the exact conditions to get a refund based on the information from the website. return it as a string. ' \
-                    '3. What is the maximum number of days for return? Summarize the exact conditions based on the information from the website. return it as a string.'
-    CRYPTO: str = 'From the information in this website answer the following question. Does the platform use ' \
-                 'blockchain to transfer the crypto or the crypto transfer is done on it’s own platform? if the ' \
-                 'merchant industry is not crypto return NULL. ' \
-                 'Return the answer as a json in this format: {crypto_transfers: blockchain\internal system}'
-    LIABILITY: str = 'Based on the information on the website, who takes liability on the following topics? ' \
-                     'Chargebacks (fraud transactions and service), delivery issues and product quality? ' \
-                     'Return the answer as a json in this format: ' \
-                     '{Chargebacks: X, delivery_issues: X, quality: X}. ' \
-                     'If this information is missing, replace X with NULL.'
-
-
-class ResponseCategories:
-    OFFERINGS: List[str] = field(default_factory=lambda: ['physical goods, digital goods, software, in-person services, in person services, personal banking, '
-                      'payment facilitation, investment services, accommodation, top up services, '
-                      'crypto currencies, gaming, gambling, nfts, hotels, car rentals, flights, tickets'])
-    INDUSTRIES: List[str] = ['retail', 'financial services', 'travel', 'gaming', 'crypto', 'software subscription', 'gambling', 'ticketing', 'delivery']
--- automatic_onboarding/get_links.py
+++ automatic_onboarding/get_links.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -13,24 +13,18 @@ options.add_argument("--window-size=1920x1080")
 options.add_argument("--disable-features=VizDisplayCompositor")
 
 
-def filter_links(links_list: List[str]):
-    word_list = ["terms", "refund", "cancel", "info", "about", "faq", "policy", "policies", "offerings", "shipping", "store", "contact"]
-
-    # Function to check if a string contains any word from the word list
-    def contains_word(string, word_list):
-        return any(word in string for word in word_list)
-
-    # Filter strings that contain at least one word from the word list and add the parent link
-    urls = [string for string in links_list if contains_word(string, word_list) and ".pdf" not in string]
-    return urls
-
-
-def convert_to_dict(links_list: List[str], website_link):
+def convert_to_apify_urls_dict(links_list: List[str], website_link: str) -> List[Dict[str, str]]:
+    """
+    Converts the website link, and the list of child links to a list of dictionaries, as required to use Apify
+    :param links_list: list of urls
+    :param website_link: single url
+    :return: List as described
+    """
     urls = [{"url": website_link}] + [{"url": string} for string in links_list]
     return urls
 
 
-def get_links(website_link: str, max_links = 10) -> Tuple[List, str]:
+def get_links(website_link: str) -> Tuple[List, str]:
     """
     get all sub-URLs in a given URL with maximum depth of 1
     :param website_link: parent URL
@@ -42,27 +36,31 @@ def get_links(website_link: str, max_links = 10) -> Tuple[List, str]:
     list_links = []
     source = "selenium"
 
+    # take all the pages with based on the css string
     if len(list_links) == 0:
         driver = webdriver.Chrome(options=options)
         driver.get(website_link)
         list_links = [element.get_attribute("href") for element in driver.find_elements(By.CSS_SELECTOR, css_string)]
         driver.quit()
     print(f"len(list_links) 1: {len(list_links)}")
+
+    # remove pages with irrelevant signs, sinse they are usually child pages of what we need
     chars = ['=', '?', '&', '%']
     list_links = [link for link in list_links if all(char not in link for char in chars)]
-
     print(f"len(list_links) 2: {len(list_links)}")
+
     # remove duplicates
     if len(list_links) > 0:
         list_links = list(set(list_links))
-    res_links = []
     print(f"len(list_links) 3: {len(list_links)}")
+
     # take maximum two from each keyword
+    res_links = []
     for key_word in key_words:
         res_links.extend(sorted([link for link in list_links if key_word in link], key=len)[:2])
     list_links = list(set(res_links))
     print(f"len(list_links) 4: {len(list_links)}")
     print(list_links)
 
-    links = convert_to_dict(list_links, website_link)
+    links = convert_to_apify_urls_dict(list_links, website_link)
     return links, source
