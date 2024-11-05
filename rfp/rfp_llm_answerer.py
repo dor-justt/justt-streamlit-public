e2114804a068d8e69e8e113e1d8e99156b39959e
Dor <dor@Dor-B-Mac.local>
2024-11-05 11:33:00 +0200

    .

--- rfp/rfp_llm_answerer.py
+++ rfp/rfp_llm_answerer.py
@@ -10,7 +10,7 @@ class RFPLlmAnswerer:
     SYSTEM_PROMPT = "I work at Justt.ai. A fintech company that handles chargebacks automatically for merchants. " \
              "I want you to fill the best answer you can on my behalf to questions for an RFP for new merchants. " \
              "I will supply previous questions and answers we made for other merchants, and want you to answer the last one. " \
-                    "Reply only the answer with additions."
+                    "Reply only the answer. Do not use the names of other merchants."
 
     def __init__(self, openai_api_key: str = None):
         openai_api_key = openai_api_key if openai_api_key is not None else os.getenv(key='OPENAI_API_KEY')
